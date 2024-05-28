1c749a5c7a52f69456a69542889a45f6a9315a7b
ashsameeh <ash_sameeh@hotmail.com>
2024-05-28 15:42:42 -0400

    Update tenderr.py

--- tenderr.py
+++ tenderr.py
@@ -127,14 +127,19 @@ def identify_table_prompt(user_query):
     return prompt
 
 def make_output(user_query):
-    try:
-        detailed_prompt = identify_table_prompt(user_query)
-        agent_executor = create_sql_agent(llm=llm,verbose=True,db=db)
-        result = agent_executor.run(detailed_prompt)
-        final_answer = result['output']
-        return final_answer
-    except Exception as e:
-        return "I'm sorry, I encountered an error while trying to answer your question. Please try again later."
+    # try:
+    #     detailed_prompt = identify_table_prompt(user_query)
+    #     agent_executor = create_sql_agent(llm=llm,verbose=True,db=db)
+    #     result = agent_executor.run(detailed_prompt)
+    #     final_answer = result['output']
+    #     return final_answer
+    # except Exception as e:
+    #     return "I'm sorry, I encountered an error while trying to answer your question. Please try again later."
+    detailed_prompt = identify_table_prompt(user_query)
+    agent_executor = create_sql_agent(llm=llm,verbose=True,db=db)
+    result = agent_executor.run(detailed_prompt)
+    final_answer = result['output']
+    return final_answer
 
 
 # user_query = "What is the acknowledgment status for tender with id NB32056724_21525?"
