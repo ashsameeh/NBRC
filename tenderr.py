a5b73bd47b58821623fb8762a7de9294525be499
ashsameeh <ash_sameeh@hotmail.com>
2024-05-28 15:10:24 -0400

    Update tenderr.py

--- tenderr.py
+++ tenderr.py
@@ -122,8 +122,8 @@ def identify_table_prompt(user_query):
 def make_output(user_query):
     # try:
         detailed_prompt = identify_table_prompt(user_query)
-        agent_executor = create_sql_agent(llm, db=db, verbose=True)
-        result = agent_executor.invoke(detailed_prompt)
+        agent_executor = create_sql_agent(verbose=True,toolkit=SQLDatabaseToolkit(db=db, llm=llm))
+        result = agent_executor.run(detailed_prompt)
         final_answer = result['output']
         return final_answer
     # except Exception as e:
