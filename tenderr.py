207243d901f17f35be2048b6360830dee11586af
ashsameeh <ash_sameeh@hotmail.com>
2024-05-28 15:50:38 -0400

    Update tenderr.py

--- tenderr.py
+++ tenderr.py
@@ -135,8 +135,8 @@ def make_output(user_query):
     #     return final_answer
     # except Exception as e:
     #     return "I'm sorry, I encountered an error while trying to answer your question. Please try again later."
-    # detailed_prompt = identify_table_prompt(user_query)
-    # agent_executor = create_sql_agent(llm=llm,verbose=True,db=db)
+    detailed_prompt = identify_table_prompt(user_query)
+    agent_executor = create_sql_agent(llm=llm,verbose=True,db=db)
     # result = agent_executor.run(detailed_prompt)
     # final_answer = result['output']
     final_answer="123"
